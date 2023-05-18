3d480da948e7ca7d2a1cba879aebf947f67d1d5c
jjc1812 <josejcoronel98@gmail.com>
2023-05-17 22:14:08 -0300

    fisrt

---
+++ js/dashboard.js
@@ -0,0 +1,23 @@
+function hoverAmount () {
+    document.getElementById("monto").innerHTML = "$ 100.000";
+}
+
+function hideAmount () {
+    document.getElementById("monto").innerHTML = "$ *******";
+}
+
+function hoverAlias () {
+    document.getElementById("alias").innerHTML = "CASA.ALMA.CAMA";
+}
+
+function hideAlias () {
+    document.getElementById("alias").innerHTML = "************************";
+}
+
+function hoverAccount () {
+    document.getElementById("cuenta").innerHTML = "11/9347436473/1";
+}
+
+function hideAccount () {
+    document.getElementById("cuenta").innerHTML = "*****************";
+}
